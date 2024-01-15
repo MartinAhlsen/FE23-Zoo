fef2d5de30149c8c4cb89ec1dc4b90c65daede57
davidboll <david.boll@me.com>
2024-01-15 13:49:27 +0100

    Adds bird-object

---
+++ scripts/birds.js
@@ -0,0 +1,12 @@
+$(() => {
+  let cassowary = {
+    lifespan: "20 years",
+    group: "bird",
+    food: "Plants matter like fruit, insects and small animals like mice amd lizards",
+    description:
+      "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida",
+    length: "1.7m",
+    weight: "44kg",
+    found: "Queensland",
+  };
+});

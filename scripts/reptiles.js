eb2cb211552490142d262f122f3496ebbf0104d9
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-01-17 11:02:13 +0100

    add some css reptile

--- scripts/reptiles.js
+++ scripts/reptiles.js
@@ -1,45 +1,53 @@
 $( () => {
-/*     let reptiles = {
-        lifespan: "20 years",
-        group: "reptiles",
-        food: "Plants matter like fruit, insects and small animals like mice amd lizards",
-        description: "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida",
-        length: "1.7m",
-        weight: "44kg",
-        found: "Queensland"
-    }
- */
 
-    function Reptile(lifespan, group, food, description, length, weigh, found) {
+    function Reptile(name, lifespan, group, food, description, length, weight, found, thumbnail, imgLarge) {
+        this.name = name,
         this.lifespan = lifespan,
         this.group = group,
         this.food = food,
         this.description = description,
         this.length = length,
-        this.weigh = weigh,
+        this.weight = weight,
         this.found = found,
+        this.thumbnail = thumbnail,
+        this.imgLarge = imgLarge,
 
-        this.createCountryElement = function() {         
+        this.createReptileElement = function() {         
             $('.main-content').append(`
-            <div class="country">
-                <h3>${this.group}</h3>
-                <p> The capital of ${this.name} is ${this.capitalCity}.</p>
-                <p> They speak ${this.language} and they spend ${this.currency}</p>
+            <div class="reptile">
+                <h3>${this.name}</h3>
+                <img src="${this.thumbnail}" alt="${this.name}" width="100" height="100">
+                <p>Group: ${this.group}</p>
+                <p>Lifespan: ${this.lifespan}</p>
+                <p>Food: ${this.food}</p>
+                <p>Description: ${this.description}</p>
+                <p>Length: ${this.length}</p>
+                <p>Weight: ${this.weight}</p>
+                <p>Found: ${this.found}</p>
+                <img src="${this.imgLarge}" alt="${this.name}" width="1000" height="600">
             </div>
             `)
         }
     }
 
-    let reptiles = new Reptile(
+    let lizard = new Reptile(
+        'Frilled-Neck lizard',
         '20 years',
-        'reptiles',
-        'Plants matter like fruit, insects and small animals like mice amd lizards',
-        'It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida',
-        '1.7m',
-        '44kg',
-        'Queensland'
+        'Reptile',
+        'Small insects and spiders',
+        'When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles its head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run without stopping or looking back until it reaches the safety of a tree.',
+        '90 cm',
+        '1 kg',
+        'Northern Australia',
+        '../images/frill-neck-lizard_thumb.jpg',
+        '../images/frill-neck-lizard_large.jpg'
     )
 
+       let reptileArray = [lizard];
 
+    $(reptileArray).each((index, reptile) => {
+        reptile.createReptileElement(); 
+    })
 
+    
 })
